06fa19615727d109a941b3ec5f2efaf6f2cf5641
Echizen1605 <980561079@qq.com>
2019-03-27 21:45:07 +0800

    Fix: 修复鼠标单击显示黑点的问题，修复鼠标光标不能变化的问题

--- qt_opencv_qq.py
+++ qt_opencv_qq.py
@@ -87,6 +87,7 @@ class myLabel(QLabel):
 		# 鼠标按下事件
 		# adjust_flag和drag_flag分别是对矩形框大小调整和矩形框位置调整的标志位，
 		# 当其中有任何一个为True时，表明矩形框已经绘制完成，否则表明当前事件为准备绘制
+		print(self.adjust_flag, self.drag_flag)
 		if not (self.adjust_flag or self.drag_flag):
 			self.draw_flag = True
 			self.start_x = event.x()
@@ -122,6 +123,10 @@ class myLabel(QLabel):
 	def mouseReleaseEvent(self,event):
 		if self.draw_flag:
 			self.draw_flag = False
+			# 单击鼠标左键时删除当前矩形框，并在左上角画长宽为0的框
+			if self.start_x == self.end_x and self.start_y == self.end_y:
+				self.start_x = self.start_y = self.end_x = self.end_y = 0
+				self.update()
 		else:
 			if self.adjust_flag:
 				self.left = False
@@ -130,13 +135,41 @@ class myLabel(QLabel):
 				self.bottom = False
 				self.adjust_flag = False
 			if self.drag_flag:
+				self.drag_flag = False
 				if self.drag_rectangle:
 					self.drag_rectangle = False
+	# 判断矩形框边长的四个中点(all_points)是否在传入的四个参数围成的矩形框中
+	def judge_cursor_on_line(self, current_x_min, current_x_max, current_y_min, current_y_max):
+		flag = False
+		all_points = self.get_four_point()
+		if abs(all_points[0][0] - current_x_min) + abs(all_points[0][0] - current_x_max) <= 6:
+			if abs(all_points[0][1] - current_y_min) + abs(all_points[0][1] - current_y_max) <= 6 \
+			 		or abs(all_points[1][1] - current_y_min) + abs(all_points[1][1] - current_y_max) <= 6:
+				self.setCursor(Qt.SizeVerCursor)
+				self.adjust_flag = True
+				self.drag_flag = False
+				flag = True
+			# else不可省，因为可能会出现内层if条件不满足但是外层if条件满足的情况
+			# 这样就会造成一个情况，x在指定范围内，但是y超出了范围，结果就是鼠标光标不会发生改变
+			else:
+				self.adjust_flag = False
+				self.drag_flag = False
+		elif abs(all_points[2][1] - current_y_min) + abs(all_points[2][1] - current_y_max) <= 6:
+			if abs(all_points[2][0] - current_x_min) + abs(all_points[2][0] - current_x_max) <= 6 \
+			 		or abs(all_points[3][0] - current_x_min) + abs(all_points[3][0] - current_x_max) <= 6:
+				self.setCursor(Qt.SizeHorCursor)
+				self.adjust_flag = True
+				self.drag_flag = False
+				flag = True
+			# else不可省，同上
+			else:
+				self.adjust_flag = False
+				self.drag_flag = False
+		return flag
 	def mouseMoveEvent(self,event):
 		# 当鼠标移动到矩形框边的中点或鼠标处于矩形框内部时，鼠标光标会发生改变
 		# 此时会分别触发adjust_flag和drag_flag一级标志位
 		# 当前面的条件满足时，按动鼠标才会触发二级标志位([left, top, right, bottom], [drag_rectangle])
-		all_points = self.get_four_point()
 		current_x_min, current_x_max = event.x() - 3, event.x() + 3
 		current_y_min, current_y_max = event.y() -3, event.y() + 3
 		# 使用鼠标绘制矩形框
@@ -147,7 +180,7 @@ class myLabel(QLabel):
 			# 重新绘制图像
 			self.update()
 		# 使用鼠标调整矩形框大小
-		elif self.adjust_flag:
+		elif self.left or self.right or self.top or self.bottom:
 			if self.left:
 				self.start_x = event.x()
 			elif self.right:
@@ -172,41 +205,22 @@ class myLabel(QLabel):
 			self.update()
 		# 上面条件均不满足，即鼠标不按下的情况下移动
 		else:
-			if abs(event.x() - self.start_x) + abs(event.x() - self.end_x) <= abs(self.start_x - self.end_x) and \
-				abs(event.y() - self.start_y) + abs(event.y() - self.end_y) <= abs(self.start_y - self.end_y):
-				self.setCursor(Qt.SizeAllCursor)
-				self.drag_flag = True
+			# 判断矩形框四个边的中点是否在以当前鼠标光标位置为中心，边长为6的邻域内
+			# 问题转换为判断一个点是否在一个矩形内
+			# 对于x的判断，可以将x与起点和终点分别求距离，再求和，如果等于矩形的宽，则表明该点的x处于矩形的水平范围内
+			# 对于y的判断，同上
+			if self.judge_cursor_on_line(current_x_min, current_x_max, current_y_min, current_y_max):
+				pass
 			else:
-				self.drag_flag = False
-				# 判断矩形框四个边的中点是否在以当前鼠标光标位置为中心，边长为6的邻域内
-				# 问题转换为判断一个点是否在一个矩形内
-				# 对于x的判断，可以将x与起点和终点分别求距离，再求和，如果等于矩形的宽，则表明该点的x处于矩形的水平范围内
-				# 对于y的判断，同上
-				if abs(all_points[0][0] - current_x_min) + abs(all_points[0][0] - current_x_max) <= 6:
-					if abs(all_points[0][1] - current_y_min) + abs(all_points[0][1] - current_y_max) <= 6 \
-					 		or abs(all_points[1][1] - current_y_min) + abs(all_points[1][1] - current_y_max) <= 6:
-						self.setCursor(Qt.SizeVerCursor)
-						self.adjust_flag = True
-					else:
-						if self.left or self.right or self.top or self.bottom:
-							pass
-						else:
-							self.setCursor(Qt.CrossCursor)
-							self.adjust_flag = False
-				elif abs(all_points[2][1] - current_y_min) + abs(all_points[2][1] - current_y_max) <= 6:
-					if abs(all_points[2][0] - current_x_min) + abs(all_points[2][0] - current_x_max) <= 6 \
-					 		or abs(all_points[3][0] - current_x_min) + abs(all_points[3][0] - current_x_max) <= 6:
-						self.setCursor(Qt.SizeHorCursor)
-						self.adjust_flag = True
-					else:
-						if self.left or self.right or self.top or self.bottom:
-							pass
-						else:
-							self.setCursor(Qt.CrossCursor)
-							self.adjust_flag = False
-				else:
-					self.setCursor(Qt.CrossCursor)
+				if abs(event.x() - self.start_x) + abs(event.x() - self.end_x) <= abs(self.start_x - self.end_x) and \
+					abs(event.y() - self.start_y) + abs(event.y() - self.end_y) <= abs(self.start_y - self.end_y):
+					self.setCursor(Qt.SizeAllCursor)
+					self.drag_flag = True
 					self.adjust_flag = False
+				else:
+					self.drag_flag = False
+					self.adjust_flag = False
+					self.setCursor(Qt.CrossCursor)
 	def paintEvent(self, event):
 		# 每次调用update时会触发该函数
 		super(myLabel, self).paintEvent(event)
